7e593059f25342047914b4eff872ed364389c926
Jennapiro <39099694+Jennapiro@users.noreply.github.com>
2018-05-24 12:43:28 -0400

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,13 @@
+class Employee:
+
+	meetings = []
+
+	def __init__(self, fn = "", ln = ""):
+		self.first_name = fn
+		self.last_name = ln
+
+	def schedule(self, meeting):
+		self.meetings.append(meeting)
+		
+		
+		
